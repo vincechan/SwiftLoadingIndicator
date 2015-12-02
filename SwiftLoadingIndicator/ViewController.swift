4cffa2858d4e4e15d817ebd7e9b5fb56495b23b2
Vince Chan <vince.yp.chan@gmail.com>
2015-12-02 01:59:24 -0800

    Initial Commit

---
+++ SwiftLoadingIndicator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftLoadingIndicator
+//
+//  Created by Vince Chan on 12/2/15.
+//  Copyright © 2015 Vince Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
